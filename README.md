ac53e93cdef4d5557fca2a473f920b3079813c48
Cubic <hourui.me@gmail.com>
2017-11-26 16:40:55 +0800

    first init

---
+++ README.md
@@ -0,0 +1,11 @@
+homebrew-libsecp256k1
+=====================
+
+Thanks to [bitcoin](https://github.com/bitcoin-core/secp256k1 "Optimized C library for EC operations on curve secp256k1")
+
+Usage.
+-----------------
+``` bash
+brew tap cuber/homebrew-libsecp256k1
+brew install libsecp256k1
+```
---
+++ libsecp256k1.rb
@@ -0,0 +1,41 @@
+class Libsecp256k1 < Formula
+  desc "Optimized C library for EC operations on curve secp256k1 "
+  homepage "https://github.com/bitcoin/secp256k1"
+  url "https://github.com/bitcoin/secp256k1.git",
+    :revision => "6ad5cdb42a1a8257289a0423d644dcbdeab0f83c"
+  version "0.1"
+
+  option :universal
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+  depends_on "libtool" => :build
+  depends_on "gmp" => :optional
+
+  def install
+    if build.universal?
+      ENV.universal_binary
+    end
+    system "./autogen.sh"
+    system "./configure", "--prefix=#{prefix}"
+    system "make", "install"
+  end
+
+  test do
+    (testpath/"test.c").write <<-EOS.undent
+      #include <secp256k1.h>
+      int main()
+      {
+        secp256k1_context *ctx =
+          secp256k1_context_create(SECP256K1_CONTEXT_VERIFY |
+                                   SECP256K1_CONTEXT_SIGN);
+        if (ctx) {
+          secp256k1_context_destroy(ctx);
+          return 0;
+        }
+        return 1;
+      }
+    EOS
+    system ENV.cc, "test.c", "-I#{include}", "-L#{lib}", "-lsecp256k1", "-o", "test"
+    system "./test"
+  end
+end
